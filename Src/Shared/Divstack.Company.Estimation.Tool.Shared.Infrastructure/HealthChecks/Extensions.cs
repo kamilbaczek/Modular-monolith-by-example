1a0d771117ede0256a32ce7ba60cd7a40218b88e
Kamil Bączek <kamilbaczek@divstack.pl>
2022-03-23 21:16:34 +0100

    [Priorities]
    - removed configuration

---
+++ Src/Shared/Divstack.Company.Estimation.Tool.Shared.Infrastructure/HealthChecks/Extensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Divstack.Company.Estimation.Tool.Shared.Infrastructure.HealthChecks;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+internal static class Extensions
+{
+    internal static IServiceCollection AddHealthChecks(this IServiceCollection services)
+    {
+        services.AddHealthChecks();
+
+        return services;
+    }
+
+    internal static void UseHealthChecks(this IApplicationBuilder app)
+    {
+    }
+}
