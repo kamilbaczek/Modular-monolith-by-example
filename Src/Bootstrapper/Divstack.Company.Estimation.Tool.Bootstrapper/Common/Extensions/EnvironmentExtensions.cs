b27473c8b176769de94d6d99969d323afa558f76
Kamil Bączek <kamilbaczek@divstack.pl>
2022-04-11 21:49:18 +0200

    [Configuration]
    - changed dev name

--- Src/Bootstrapper/Divstack.Company.Estimation.Tool.Bootstrapper/Common/Extensions/EnvironmentExtensions.cs
+++ Src/Bootstrapper/Divstack.Company.Estimation.Tool.Bootstrapper/Common/Extensions/EnvironmentExtensions.cs
@@ -3,9 +3,10 @@
 internal static class EnvironmentExtensions
 {
     private const string LocalEnvironmentName = "Local";
+    private const string DevEnvironmentName = "Dev";
 
     internal static bool IsForDevs(this IHostEnvironment hostContext)
     {
-        return hostContext.IsEnvironment(LocalEnvironmentName) || hostContext.IsDevelopment();
+        return hostContext.IsEnvironment(LocalEnvironmentName) || hostContext.IsEnvironment(DevEnvironmentName);
     }
 }
