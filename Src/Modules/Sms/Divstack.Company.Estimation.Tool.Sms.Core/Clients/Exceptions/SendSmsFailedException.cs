bc705741ff39657079e41a5e37cfc7799e9922be
Kamil Bączek <kamilbaczek@divstack.pl>
2022-04-22 22:46:07 +0200

    [Sms]
    - improve sender

---
+++ Src/Modules/Sms/Divstack.Company.Estimation.Tool.Sms.Core/Clients/Exceptions/SendSmsFailedException.cs
@@ -0,0 +1,10 @@
+﻿namespace Divstack.Company.Estimation.Tool.Sms.Core.Clients.Exceptions;
+
+public sealed class SendSmsFailedException : InvalidOperationException
+{
+    private static string GetMessage(Exception smsClientException) => $"Send sms failed - {typeof(Exception)}:'{smsClientException.Message}'";
+
+    public SendSmsFailedException(Exception smsClientException) : base(GetMessage(smsClientException))
+    {
+    }
+}
--- Src/Modules/Sms/Divstack.Company.Estimation.Tool.Sms.Core/Clients/SmsClient.cs
+++ Src/Modules/Sms/Divstack.Company.Estimation.Tool.Sms.Core/Clients/SmsClient.cs
@@ -1,6 +1,7 @@
 ﻿namespace Divstack.Company.Estimation.Tool.Sms.Core.Clients;
 
 using Configurations.PhoneNumbers;
+using Exceptions;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
 
@@ -13,9 +14,20 @@ internal sealed class SmsClient : ISmsClient
     }
     public async Task SendAsync(string message, string to, CancellationToken cancellationToken = default)
     {
+        try
+        {
+            await SendMessage(message, to);
+        }
+        catch (Exception exception)
+        {
+            throw new SendSmsFailedException(exception);
+        }
+    }
+    private async Task SendMessage(string message, string to)
+    {
         await MessageResource.CreateAsync(
             body: message,
-            from: new PhoneNumber(_phoneNumbersConfiguration.From),
+            @from: new PhoneNumber(_phoneNumbersConfiguration.From),
             to: new PhoneNumber(to)
         );
     }
