57e036e165f435481fef6960a9e3fbe609fc79d5
Kamil Bączek <kamilbaczek@divstack.pl>
2022-06-16 20:46:10 +0200

    [HealthChecks]
    - added new health check

--- Src/Modules/Valuations/Divstack.Company.Estimation.Tool.Valuations.Persistance/HealthChecks/HealthChecksModule.cs
+++ Src/Modules/Valuations/Divstack.Company.Estimation.Tool.Valuations.Persistance/HealthChecks/HealthChecksModule.cs
@@ -4,13 +4,12 @@ using Microsoft.Extensions.DependencyInjection;
 
 internal static class HealthChecksModule
 {
-    private const string DatabaseName = "Valuations Database";
     private const string Valuations = "Valuations";
     private const string Database = "Valuations";
 
     internal static IServiceCollection AddPersistanceHealthChecks(this IServiceCollection services, string connectionString)
     {
-        // services.AddHealthChecks().AddMongoDb(connectionString, DatabaseName, null, new[] { Valuations, Database });
+        services.AddHealthChecks().AddNpgSql(connectionString, tags: new[] { Valuations, Database });
 
         return services;
     }
