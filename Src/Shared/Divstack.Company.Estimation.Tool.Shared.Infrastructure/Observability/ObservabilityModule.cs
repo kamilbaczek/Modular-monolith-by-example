c523718dbe931de0d1c1c996c341a34db70ce631
Kamil Bączek <kamilbaczek@divstack.pl>
2022-07-09 18:33:45 +0200

    fix

--- Src/Shared/Divstack.Company.Estimation.Tool.Shared.Infrastructure/Observability/ObservabilityModule.cs
+++ Src/Shared/Divstack.Company.Estimation.Tool.Shared.Infrastructure/Observability/ObservabilityModule.cs
@@ -1,13 +1,9 @@
 ﻿namespace Divstack.Company.Estimation.Tool.Shared.Infrastructure.Observability;
 
-using Azure.Telemetry;
-
 internal static class ObservabilityModule
 {
     internal static IServiceCollection AddObservability(this IServiceCollection services)
     {
-        AzureApplicationInsightsModule.AzureApplicationInsights();
-
         return services;
     }
 }
