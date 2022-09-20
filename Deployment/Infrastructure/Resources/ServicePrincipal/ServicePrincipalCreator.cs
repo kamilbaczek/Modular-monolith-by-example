d9ba885e4961cfce68af4ed33b304f60b1eeb50e
Kamil Bączek <kamilbaczek@divstack.pl>
2022-09-20 13:40:38 +0200

    - fix

--- Deployment/Infrastructure/Resources/ServicePrincipal/ServicePrincipalCreator.cs
+++ Deployment/Infrastructure/Resources/ServicePrincipal/ServicePrincipalCreator.cs
@@ -1,6 +1,7 @@
 ﻿namespace Divstack.Estimation.Tool.Deployment.Infrastructure.Resources.ServicePrincipal;
 
 using Pulumi.AzureAD;
+using Pulumi.AzureAD.Inputs;
 
 internal static class ServicePrincipalCreator
 {
@@ -11,13 +12,32 @@ internal static class ServicePrincipalCreator
 
         var application = new Application($"app-ad-{environment}", new()
         {
+            AppRoles = new ApplicationAppRoleArgs[]
+            {
+                new()
+                {
+                    AllowedMemberTypes = new[] { "Application" },
+                    Description = "Allows the application to access Estimation Tool API on behalf of the signed-in user.",
+                    DisplayName = "Estimation Tool API",
+                    Value = "estimation-tool-api",
+                },
+            },
+
             DisplayName = $"Estimation Tool - {environment}",
+            Owners = new[]
+            {
+                currentPrincipal
+            },
         });
 
         var servicePrincipal = new ServicePrincipal($"sp-ac-kv-{environment}-policy", new()
         {
             ApplicationId = application.ApplicationId,
             AppRoleAssignmentRequired = false,
+            Owners = new[]
+            {
+                currentPrincipal
+            },
         });
 
         return servicePrincipal;
